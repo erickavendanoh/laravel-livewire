3c20641f287c20c372869dc386e30e5a3d13b801
Erick Avendaño <117497394+erickavendanoh@users.noreply.github.com>
2024-06-29 14:41:27 -0600

    finalización video 2

--- laravel-livewire/resources/views/livewire/show-posts.blade.php
+++ laravel-livewire/resources/views/livewire/show-posts.blade.php
@@ -1,4 +1,9 @@
 <div>
-    <h1>Hola mundo</h1>
-    {{$titulo}}
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    {{$posts}}
 </div>
