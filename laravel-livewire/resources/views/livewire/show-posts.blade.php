0ee351cba77b8240f7b35054c4b9fdd3c94cafb2
erickavendanoh <erick-armando167@outlook.es>
2024-07-03 13:47:13 -0600

    Minuto 7.51 video 3

--- laravel-livewire/resources/views/livewire/show-posts.blade.php
+++ laravel-livewire/resources/views/livewire/show-posts.blade.php
@@ -28,23 +28,20 @@
                     @foreach ($posts as $post)
                         <tr>
                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <p class="text-gray-900 whitespace-no-wrap">Admin</p>
+                                <p class="text-gray-900 whitespace-no-wrap">{{$post->id}}</p>
                             </td>
                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                 <p class="text-gray-900 whitespace-no-wrap">
-                                    Jan 21, 2020
+                                    {{$post->title}}
                                 </p>
                             </td>
                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                 <p class="text-gray-900 whitespace-no-wrap">
-                                    43
+                                    {{$post->content}}
                                 </p>
                             </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                                    <span aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                    <span class="relative">Activo</span>
-                                </span>
+                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                             </td>
                         </tr>
                     @endforeach
@@ -52,5 +49,5 @@
             </table>
         </x-table>
     </div>
-
+    
 </div>
